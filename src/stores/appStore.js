d9e0c2acf15a9e75f865720afb105e19ea364db3
Alex Sinfarosa <as898@cornell.edu>
2017-10-06 09:46:03 -0400

    updates

--- src/stores/appStore.js
+++ src/stores/appStore.js
@@ -1,7 +1,33 @@
-// import { observable, action, computed } from "mobx";
+import { observable, action } from "mobx";
+import axios from "axios";
 
 export default class appStore {
   constructor(fetch) {
     this.fetch = fetch;
+  }
+
+  @observable isLoading = false;
+
+  @observable gridData = [];
+  @action
+  updateGridData = d => {
+    this.gridData.clear();
+    this.gridData = d;
+  };
+
+  @action
+  loadGridData() {
+    this.isLoading = true;
+
+    return axios
+      .post(`${this.protocol}//grid.rcc-acis.org/GridData`, params)
+      .then(res => {
+        console.log(res.data.data);
+        this.updateGridData(res.data.data);
+        this.isLoading = false;
+      })
+      .catch(err => {
+        console.log("Failed to load data model", err);
+      });
   }
 }
